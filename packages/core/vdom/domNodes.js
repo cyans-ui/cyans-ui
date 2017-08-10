107eedc93b086d7daff67504bbba72cf5868a74d
Luca Colonnello <luca.colonnello91@gmail.com>
2017-08-10 23:27:09 +0100

    feat(vdom): Add ability to append new nodes handling arrays and undefined nodes
    https://github.com/cyans-ui/cyans-ui/issues/16

--- packages/core/vdom/domNodes.js
+++ packages/core/vdom/domNodes.js
@@ -1,3 +1,14 @@
+import {
+  setProps,
+} from './props';
+
 export function createTextDomNode(text) {
   return document.createTextNode(text.toString());
+}
+
+export function createDomNode(node) {
+  const domElement = document.createElement(node.type);
+  setProps(domElement, node.props);
+
+  return domElement;
 }
